84b76f6842532f6e6ed6c9209f3a1c8adf9aff30
CaffeinatedDragonWare <71392168+CaffeinatedDragonWare@users.noreply.github.com>
2025-04-25 12:55:49 -0300

    Helpful scripts

---
+++ boot-movie-mover.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+mv /home/deck/homebrew/data/SDH-AnimationChanger/downloads/* /home/deck/.steam/root/config/uioverrides/movies/
---
+++ desktop_black_fix.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+rm -rf /home/deck/.local/share/kscreen
